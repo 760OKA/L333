50ee589bfd64187b33b347d0eeaf6433acbbe242
760OKA <156885058+760OKA@users.noreply.github.com>
2024-03-02 16:01:00 +0300

    Add files via upload

---
+++ Факториал рекурсии.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int recursion(int num)
+{
+    int a = num;
+    if (num == 0)
+    {
+        return 0;
+    }
+    else if (num == 1)
+    {
+        return 1;
+    }
+    return num * recursion(num - 1);
+}
+int main()
+{
+    setlocale(0, "");
+    int a;
+    cout << "Введите факториал: ";
+    cin >> a;
+    cout << "Факториал = " << recursion(a) << "." << endl;
+    int _;
+    cin >> _;
+    return 0;
+}
