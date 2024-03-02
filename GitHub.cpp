99aaeef2835f4a823c7b2c386cc9eb08da96b1e1
760OKA <156885058+760OKA@users.noreply.github.com>
2024-03-02 15:17:34 +0300

    Add files via upload

---
+++ GitHub.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    while (1) {
+        int a;
+        cout << "Введите только положительное число: ";
+        cin >> a;
+        if (a >= 0) {
+            continue;
+        }
+        else
+        {
+            cout << "Вы написали отрицательное число: ";
+        }
+        cout << a << endl;
+        break;
+    }
+}
